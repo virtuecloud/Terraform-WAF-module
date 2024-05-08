5089e3ed28bbab314f5d3cb1a4b6bfa8f478de30
Ankit Kumar Das <76767906+ankitdasgit@users.noreply.github.com>
2024-05-08 13:26:45 +0530

    Update variables.tf

--- module/WAF/variables.tf
+++ module/WAF/variables.tf
@@ -1,6 +1,14 @@
-variable "web_acl_name" {}
+variable "web_acl_name" {
+  description = "The name of the AWS WAFv2 Web ACL"
+}
 
 variable "apis" {
-type    = map(any)
-default = {}
+  description = "A map of API Gateway resources to associate with the Web ACL"
+  type        = map(object({
+    region = string
+    api_id = string
+    stage  = string
+  }))
 }
+
+
