7acd07c8ae7c16e566f19bdf19a29e05c121729c
Ankit Kumar Das <76767906+ankitdasgit@users.noreply.github.com>
2024-05-08 13:26:07 +0530

    Updated main.tf file

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
+data "aws_api_gateway_rest_api" "apis" {
+  for_each = var.apis
+  name = each.value.api_name
+}
+
 module "waf" {
   source = "../module/WAF"
-
   web_acl_name = var.web_acl_name
-  apis = var.apis
+
+  apis = {
+    for idx, api in data.aws_api_gateway_rest_api.apis : 
+    api.id => {
+      region = "ap-southeast-2" 
+      api_id = api.id
+      stage  = "dev"     
+    }
+  }
+
+
+
 }
